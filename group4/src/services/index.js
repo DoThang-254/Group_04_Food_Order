833c1d21ad19ce5a17ded76bace76115ec272b30
DoThang-254 <thangmoneo2542004@gmail.com>
2025-07-28 11:30:30 +0700

    add file

---
+++ group4/src/services/index.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: "",
+    // timeout: 1000,
+});
+
+instance.interceptors.request.use(
+    function (config) {
+        if (localStorage.getItem("token")) {
+            config.headers.Authorization = "Bearer " + localStorage.getItem("token");
+        }
+
+        return config;
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+);
+
+instance.interceptors.response.use(
+    function (response) {
+
+        return response;
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+);
+
+export default instance;
