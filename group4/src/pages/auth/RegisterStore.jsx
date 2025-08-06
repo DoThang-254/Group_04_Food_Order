3f4887f9b6200cafe4ac29218c9116e1d17cbe0e
DoThang-254 <thangmoneo2542004@gmail.com>
2025-08-06 12:30:27 +0700

    add register store

--- group4/src/pages/auth/RegisterStore.jsx
+++ group4/src/pages/auth/RegisterStore.jsx
@@ -1,38 +1,91 @@
 import { ErrorMessage, Field, Form, Formik } from 'formik';
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import * as Yup from "yup";
+import { getStoreByOwnerId, postStore } from '../../services/stores';
+import { decodeFakeToken } from '../../data/token';
+import { loginContext } from '../../context/LoginContext';
+import { updateUser } from '../../services/users';
 const RegisterStore = () => {
     const RegisterStoreSchema = Yup.object().shape({
-        password: Yup.string()
+        storeName: Yup.string()
             .required('Required'),
-        email: Yup.string()
-            .email('Required email format!')
+        storeAddress: Yup.string()
             .required('Required'),
     });
+    const [loading, setLoading] = useState(true);
+    const [info, setinfo] = useState();
+    const [msg, setMsg] = useState();
+    const { token } = useContext(loginContext);
+    useEffect(() => {
+        const decode = async () => {
+            const info = await decodeFakeToken(token);
+            if (info) {
+                setinfo(info);
+            }
+            setLoading(false);
+        };
+        decode();
+    }, [token]);
 
-    const handleRegisterStore = async (value) => {
-        const data = value
-        console.log(data);
+    if (loading) return null;
+    const handleRegisterStore = async (data) => {
+        data.state = false;
+        
+        data.ownerId = info?.id;
+        let newStore;
+        try {
+            const store = await getStoreByOwnerId(info?.id);
+            if (!store) {
+                newStore = await postStore(data);
+            }
+        } catch (error) {
+            setMsg('Something went wrong')
+            console.log(error)
+        }
+
+        try {
+            if (newStore) {
+                const check = await updateUser(info?.id, { role : 'owner' , storeId: newStore.id });
+                if (check) {
+                    setMsg('Send store request successfully!!!');
+                }
+                else {
+                    setMsg('Please Login');
+                }
+            }
+            else {
+                setMsg('You owned a store');
+            }
+
+        } catch (error) {
+            setMsg('Something went wrong')
+            console.log(error)
+        }
     }
     return (
         <div>
             <Formik
                 initialValues={{ storeName: '', storeAddress: '' }}
-                onSubmit={handleRegisterStore}
+                onSubmit={data => handleRegisterStore(data)}
                 validationSchema={RegisterStoreSchema}
             >
                 <Form>
                     <div className="mb-3">
-                        <label htmlFor="storeName" className="form-label">Email</label>
-                        <Field name="storeName" type="email" className="form-control" />
+                        <label htmlFor="storeName" className="form-label">Store Name</label>
+                        <Field name="storeName" type="text" className="form-control" />
                         <ErrorMessage name="storeName" component="div" className="text-danger" />
                     </div>
 
                     <div className="mb-3">
-                        <label htmlFor="storeAddress" className="form-label">Password</label>
+                        <label htmlFor="storeAddress" className="form-label">Store Address</label>
                         <Field name="storeAddress" type="text" className="form-control" />
                         <ErrorMessage name="storeAddress" component="div" className="text-danger" />
                     </div>
+
+                    <div className="d-grid mb-3">
+                        <button type="submit" className="btn btn-primary">Register Store</button>
+                    </div>
+                    {msg}
                 </Form>
             </Formik>
         </div>
